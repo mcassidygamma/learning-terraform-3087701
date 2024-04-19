598ea21d71ce2ee8634d537f8cdbeb13b5373b58
Martin Cassidy <martin.cassidy@gamma.co.uk>
2024-04-19 15:13:11 +0100

    updated

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [aws_security_group.blog.id]
 
+  subnet_id = module.blog_vpc.public.subnets[1]
+
   tags = {
     Name = "HelloWorld"
   }
@@ -59,4 +61,20 @@ resource "aws_security_group_rule" "blog_everything_out" {
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
