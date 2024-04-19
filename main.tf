c117e1a5db4781e098552d5b23c94ba0c3e9eeaa
Martin Cassidy <martin.cassidy@gamma.co.uk>
2024-04-19 16:12:08 +0100

    variables1

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,14 +11,14 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] # Bitnami
 }
 
-module "blog_vpc" {
+module "blogm_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "$(var.environment.network_prefix)"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
@@ -30,22 +30,22 @@ module "blog_vpc" {
   }
 }
 
-module "blog_autoscaling" {
+module "blogm_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
   
   name = "blog-martin"
-  min_size = 1
-  max_size = 2
+  min_size = var.min_size
+  max_size = var.max_size
 
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
+  vpc_zone_identifier = module.blogm_vpc.public_subnets
+  target_group_arns = module.blogm_alb.target_group_arns
+  security_groups = [module.blogm_sg.security_group_id]
   image_id = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 }
 
-module "blog_alb" {
+module "blogm_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
@@ -53,9 +53,9 @@ module "blog_alb" {
 
 load_balancer_type = "application"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  vpc_id  = module.blogm_vpc.vpc_id
+  subnets = module.blogm_vpc.public_subnets
+  security_groups = [module.blogm_sg.security_group_id]
 
   target_groups = [
     {
@@ -75,15 +75,15 @@ load_balancer_type = "application"
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
-module "blog_sg" {
+module "blogm_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
+  vpc_id  = module.blogm_vpc.vpc_id
   name    = "blog-martin"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
